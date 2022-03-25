e28fac1117a13c74342eff283536b844457b6ef6
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-03-24 23:06:43 -0800

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,4 +1,3 @@
-// creating an array and passing the number, questions, options, and answers
 let questions = [
     {
     numb: 1,
@@ -55,18 +54,4 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
